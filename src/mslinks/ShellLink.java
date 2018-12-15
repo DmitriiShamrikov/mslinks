0a300a63454ee47e7073ebaa79d8c8009936dde5
BlackOverlord <blackoverlordd@gmail.com>
2018-12-15 20:48:34 +0500

    changed version

--- src/mslinks/ShellLink.java
+++ src/mslinks/ShellLink.java
@@ -42,7 +42,7 @@ import mslinks.extra.VistaIDList;
 
 public class ShellLink {
 
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 
 	private static Map<String, String> env = System.getenv();
 	
