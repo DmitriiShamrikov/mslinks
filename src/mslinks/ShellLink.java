ba172b89ff4d419e880235ddc68a26f737b292ee
blackoverlord <blackoverlordd@gmail.com>
2020-08-24 23:05:45 +0200

    bumped version number

--- src/mslinks/ShellLink.java
+++ src/mslinks/ShellLink.java
@@ -42,7 +42,7 @@ import mslinks.extra.VistaIDList;
 
 public class ShellLink {
 
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "1.0.6";
 
 	private static Map<String, String> env = System.getenv();
 	
