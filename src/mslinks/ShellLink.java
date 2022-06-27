58608f513efe49bba17aedb575050eef7e1d0f2d
blackoverlord <blackoverlordd@gmail.com>
2022-06-27 19:02:26 +0100

    bumped version number

--- src/mslinks/ShellLink.java
+++ src/mslinks/ShellLink.java
@@ -38,7 +38,7 @@ import mslinks.extra.VistaIDList;
 
 public class ShellLink {
 
-	public static final String VERSION = "1.0.8";
+	public static final String VERSION = "1.0.9";
 	
 	private static HashMap<Integer, Class<? extends Serializable>> extraTypes = new HashMap<>(Map.of(
 		ConsoleData.signature, ConsoleData.class,
